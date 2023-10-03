ec1bb766ece7e83133715788067aab8fd0d09842
Matias de teran <mati.deteran32@gmail.com>
2023-10-03 20:22:46 -0300

    enve-react-api

--- src/conteiners/MostrarInfo.jsx
+++ src/conteiners/MostrarInfo.jsx
@@ -1,92 +1,67 @@
+import React, { useEffect } from "react"
 import { useState } from "react"
-import { TraerInfo } from "./TraerInfo.jsx"
 
 export const MostarInfo = () => {
 
-    const { data } = TraerInfo('https://jsonplaceholder.typicode.com/posts')
-    
-    const [info, setInfo] = useState(null)
+    const [data, setData] = useState([]);
 
-    function mostrar (){
-        return(
-            <ul>
-                {data.map((element)=>{
-                    <li key={element.id}>
-                        {element.title}
-                    </li>
-                })}
-            </ul>
-        )
+    const [comments, setComments] = useState([]);
+
+    const fetchPosts = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => { setData(json) })
+    }
+
+    const FetchComments = () => {
+        useEffect(() => {
+            fetch(`https://jsonplaceholder.typicode.com/comments?postId=1`)
+                .then(response => response.json())
+                .then(json => { setComments(json) })
+            console.log(comments);
+        }, [])
+
     }
 
 
-    return(
-        <>
-            <button onClick={()=>{setInfo(mostrar())}}>presioname para recibir la informacion</button>
 
-            <div>{info}</div>
+    // useEffect(() => {
+    //   fetchData();
+    // }, [])
+
+
+    return (
+        <>
+            <button onClick={fetchPosts}>Posts</button>
+            <div>
+                {data.map((item) => (
+                    <div key={item.id} className="divPosts">
+                        <h1>Title: {item.title}</h1>
+                        <p>Body: {item.body}</p>
+                        {useEffect(() => {
+                            fetch(`https://jsonplaceholder.typicode.com/comments?postId=1`)
+                                .then(response => response.json())
+                                .then(json => { setComments(json) })
+                            console.log(comments);
+                        }, [data])}
+
+                        <button onClick={() => {
+                            return (
+                                comments.map((element) => (
+                                    <div key={element.id}>
+                                        <h2>post numero: {element.id}</h2>
+                                        <p>name: {element.name}</p>
+                                        <p>email: {element.email}</p>
+                                        <p>body: {element.body}</p>
+                                    </div>
+                                )))
+                        }}>Comments</button>
+
+                    </div>
+                ))}
+            </div>
         </>
     )
-
-
-
-
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export const MostarInfo = () => {
-
-//     const { data } = TraerInfo('https://jsonplaceholder.typicode.com/posts')
-
-//     function Mostrar() {
-//         return (
-//             <ul>
-//                 {data.map(element => {
-//                     <div>
-//                         <h1 key={element.id}>{element.title}</h1>
-
-//                         <h2 key={element.id}>{element.body}</h2>
-//                     </div>
-//                 })}
-//             </ul>
-//         )
-
-//     }
-
-//     function Hola (){
-//         console.log(data);
-//     }
-
-//     return (
-        
-//         <>
-//             {/* <button onClick={Mostrar}>prensioname</button> */}
-//             <Hola />
-
-//         </>
-//     )
-// }
-
-
-// {/* <TraerInfo url = 'https://jsonplaceholder.typicode.com/posts'/> */ }
--- src/conteiners/TraerInfo.jsx
+++ src/conteiners/TraerInfo.jsx
@@ -4,12 +4,10 @@ export const TraerInfo = (url) => {
 
     const [data, setData] = useState(null);
 
-    useEffect(() => {
         fetch(url)
             .then(response => response.json())
-            .then(data => setData(data))
-    }, [])
+            .then(json => setData(json))
 
-    // console.log(data);
+    console.log(data);
     return { data };
 }
