a4c11552fa579924d53412262452e21261b9ada8
matias de teran <mati.deteran32@gmail.com>
2023-10-04 00:38:52 -0300

    enve-react-apis

--- src/conteiners/MostrarInfo.jsx
+++ src/conteiners/MostrarInfo.jsx
@@ -1,67 +1,47 @@
-import React, { useEffect } from "react"
-import { useState } from "react"
+import React, { useEffect } from "react";
+import { useState } from "react";
+import "../styles/conteiners.css";
 
 export const MostarInfo = () => {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState([]);
+  const [comments, setComments] = useState([]);
 
-    const [comments, setComments] = useState([]);
+  const fetchPosts = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+      });
+  };
 
-    const fetchPosts = () => {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-            .then(response => response.json())
-            .then(json => { setData(json) })
-    }
+  const FetchComments = () => {
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=1`)
+      .then((response) => response.json())
+      .then((json) => {
+        setComments(json);
+      });
+    console.log(comments);
+  };
 
-    const FetchComments = () => {
-        useEffect(() => {
-            fetch(`https://jsonplaceholder.typicode.com/comments?postId=1`)
-                .then(response => response.json())
-                .then(json => { setComments(json) })
-            console.log(comments);
-        }, [])
+  // useEffect(() => {
+  //   fetchData();
+  // }, [])
 
-    }
+  return (
+    <>
+      <button onClick={fetchPosts} className="botonPost">
+        Bring posts
+      </button>
 
-
-
-    // useEffect(() => {
-    //   fetchData();
-    // }, [])
-
-
-    return (
-        <>
-            <button onClick={fetchPosts}>Posts</button>
-            <div>
-                {data.map((item) => (
-                    <div key={item.id} className="divPosts">
-                        <h1>Title: {item.title}</h1>
-                        <p>Body: {item.body}</p>
-                        {useEffect(() => {
-                            fetch(`https://jsonplaceholder.typicode.com/comments?postId=1`)
-                                .then(response => response.json())
-                                .then(json => { setComments(json) })
-                            console.log(comments);
-                        }, [data])}
-
-                        <button onClick={() => {
-                            return (
-                                comments.map((element) => (
-                                    <div key={element.id}>
-                                        <h2>post numero: {element.id}</h2>
-                                        <p>name: {element.name}</p>
-                                        <p>email: {element.email}</p>
-                                        <p>body: {element.body}</p>
-                                    </div>
-                                )))
-                        }}>Comments</button>
-
-                    </div>
-                ))}
-            </div>
-        </>
-    )
-}
-
-
+      <div className="div-general">
+        {data.map((item) => (
+          <div key={item.id} className="divPosts">
+            <h2 className="titulos">Title: {item.title}</h2>
+            <p className="cuerpo-post">Body: {item.body}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { MostarInfo } from './conteiners/MostrarInfo'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MostarInfo } from "./conteiners/MostrarInfo";
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <MostarInfo />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
